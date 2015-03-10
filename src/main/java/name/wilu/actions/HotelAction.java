1808e6749de3fd54851305bd881d6c0ff4c3f252
Piotr Wilczek <piotr.wilczek@wilu.name>
2015-03-10 21:27:48 +0100

    Added model to action

--- src/main/java/name/wilu/actions/HotelAction.java
+++ src/main/java/name/wilu/actions/HotelAction.java
@@ -2,12 +2,17 @@ package name.wilu.actions;
 
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import name.wilu.model.Network;
+import name.wilu.service.HotelService;
 import org.apache.struts2.convention.annotation.Action;
 
 @Action(value = "conversation")
-public class HotelAction extends ActionSupport{
+public class HotelAction extends ActionSupport implements ModelDriven<Network> {
 
+    private final HotelService service = new HotelService();
 
-
-
+    public Network getModel() {
+        return service.readNetwork();
+    }
 }
